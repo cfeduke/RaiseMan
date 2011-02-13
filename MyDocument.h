b0c40299b92bafe18e3657dd122e10ff42fd25c7
Charles Feduke <charles.feduke@gmail.com>
2011-02-13 16:12:40 -0500

    Initial commit

---
+++ MyDocument.h
@@ -0,0 +1,15 @@
+//
+//  MyDocument.h
+//  RaiseMan
+//
+//  Created by Charles Feduke on 2/13/11.
+//  Copyright 2011 Mobile Posse. All rights reserved.
+//
+
+
+#import <Cocoa/Cocoa.h>
+
+@interface MyDocument : NSDocument
+{
+}
+@end
---
+++ main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  RaiseMan
+//
+//  Created by Charles Feduke on 2/13/11.
+//  Copyright 2011 Mobile Posse. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **) argv);
+}
